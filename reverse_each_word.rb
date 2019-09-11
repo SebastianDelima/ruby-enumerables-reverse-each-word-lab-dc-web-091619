ba7d7d28c74087c8a8e19ec655caf8551944db94
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 20:31:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  p string.split
-  string.each{|change| change.reverse}
+  array = string.split
+  array.each{|change| change.reverse}
 end
 
 reverse_each_word("Hello there, and how are you?")
