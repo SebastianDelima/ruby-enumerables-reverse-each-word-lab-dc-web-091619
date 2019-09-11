a189bd9a1043c7b0476e911133d612f7624fb752
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 20:40:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   
   array = string.split
-  array.each{|change| new_array.push(change.reverse)}
-  return  new_array.join(' ')
+  array.collect{|change| new_array.push(change.reverse)}
+  return  new_array
 end
 
